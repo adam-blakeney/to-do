cb4a0112c1499e68b2091d33a58a6e99bf3a22b2
adam-blakeney <adam.blakeney@outlook.com>
2022-09-07 11:59:47 +0000

    prepare for deploy

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +75,18 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': dj_database_url.parse('postgres://prdeumkcsxrmep:f95841cc9eab3429cb03587ac26bae8a81c5f0fce588996a7e74ca2916da6b3d@ec2-54-204-241-136.compute-1.amazonaws.com:5432/d8cs8c6ranjdce')
     }
-}
+
 
 
 # Password validation
