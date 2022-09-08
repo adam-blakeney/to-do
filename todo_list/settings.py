25287c998a748f8d41b72dce95eb2d01ca011c0f
adam-blakeney <adam.blakeney@outlook.com>
2022-09-08 14:09:00 +0000

    add env and host

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -15,6 +15,8 @@ import os
 import dj_database_url
 
 
+development = os.environ.get('DEVELOPMENT' ,False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,10 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-rpl)8cb-3oyteo=(^^=@&2go^6#8ad^kk478-6(jyx$paarvhf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
-
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME', 'todo-2326-app.herokuapp.com')]
 
 # Application definition
 
@@ -77,16 +81,18 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
